9ba1b53c474c0fd0804b2ce2aaf11ad90b492746
arifraj199 <karif9514@gmail.com>
2024-03-25 19:44:59 +0600

    banner section added

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import bannerImg from "../../assets/Images/banner.png";
+
+const Banner = () => {
+  return (
+    <div className="bg-[#1313130D] flex items-center justify-center p-20 gap-20">
+      <div className="w-1/2">
+        <h1 className="text-[56px] mb-12">
+          Books to freshen up your bookshelf
+        </h1>
+        <Link to="/listed-books">
+          <button className="btn btn-accent">View The List</button>
+        </Link>
+      </div>
+      <div>
+        <img src={bannerImg} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
