802935d950ec0222295710aa4156d21f34f4d931
arifraj199 <karif9514@gmail.com>
2024-03-27 14:19:02 +0600

    navbar design and responsive design added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <div className="bg-[#1313130D] flex items-center justify-center p-20 gap-20">
       <div className="w-1/2">
-        <h1 className="text-[56px] mb-12">
+        <h1 className="text-[56px] mb-12 playfair-display">
           Books to freshen up your bookshelf
         </h1>
         <Link to="/listed-books">
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,30 @@
 import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   const menuLink = (
     <>
-      <li>
+      <li className="text-sm lg:text-lg font-normal text-[#131313CC]">
         <NavLink to="/">Home</NavLink>
       </li>
-      <li>
+      <li className="text-sm lg:text-lg font-normal text-[#131313CC]">
         <NavLink to="/listed-books">Listed Books</NavLink>
       </li>
-      <li>
+      <li className="text-sm lg:text-lg font-normal text-[#131313CC]">
         <NavLink to="/pages-read">Pages To Read</NavLink>
       </li>
     </>
   );
   return (
-    <nav>
+    <nav className="mb-14 mt-8">
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost lg:hidden pr-2 pl-0"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -42,14 +47,20 @@ const Header = () => {
               {menuLink}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">Books House</a>
+          <a className="btn btn-ghost pl-0 text-xl lg:text-[28px] font-bold">
+            Books House
+          </a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{menuLink}</ul>
         </div>
-        <div className="navbar-end gap-3">
-          <a className="btn btn-accent">Sign In</a>
-          <a className="btn btn-primary">Sign Out</a>
+        <div className="navbar-end gap-2 lg:gap-3 w-3/5">
+          <a className="btn bg-[#23BE0A] text-xs lg:text-lg text-white w-[80px] lg:w-28">
+            Sign In
+          </a>
+          <a className="btn bg-[#59C6D2] text-xs lg:text-lg text-white w-[85px]  lg:w-28">
+            Sign Out
+          </a>
         </div>
       </div>
     </nav>
